0588ef891f50336eb2583632220e3ff996ab9867
meytizn <zamanianmehdi23desktop@gmail.com>
2025-03-08 14:38:24 +0330

    checkbox added in todolist cp

--- app/components/optionals/TodoList.jsx
+++ app/components/optionals/TodoList.jsx
@@ -5,7 +5,11 @@ export default function TodoList({todos}) {
     <>
               {todos.map((todo) => (
             <div key={todo.uuid} className="flex flex-row justify-between bg-green-300 h-[40px] text-[20px] gap-3 items-center px-3 rounded-md">
-              <div className={`text-left ${todo.status ? "line-through text-red-400 " : ""}`}>{todo.id} - {todo.title}</div>
+              <div className={`text-left ${todo.status ? "line-through  " : ""}`}>
+
+               <input checked={todo.status} type="checkbox" onChange={()=>{}} />     {todo.id} - {todo.title}
+                
+                </div>
               <div className="w-[25%] flex flex-row justify-between">
                 <div>Edit</div>
                 <div>Delete</div>
